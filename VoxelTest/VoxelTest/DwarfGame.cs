bfcffb1af9e2eaa990ae2b5c1dd44094d9baafbb
mklingen <mklingen@localhost>
2013-08-30 15:25:21 -0400

    displaying version number

--- VoxelTest/VoxelTest/DwarfGame.cs
+++ VoxelTest/VoxelTest/DwarfGame.cs
@@ -19,10 +19,11 @@ namespace DwarfCorp
         public GraphicsDeviceManager graphics;
         public TextureManager TextureManager { get; set; }
         public static SpriteBatch SpriteBatch { get; set; }
+        public static string VersionString { get; set; }
 
         public DwarfGame()
         {
-            
+            VersionString = "1 . 0 . 21";
             Content.RootDirectory = "Content";
             StateManager = new GameStateManager(this);
             graphics = new GraphicsDeviceManager(this);
