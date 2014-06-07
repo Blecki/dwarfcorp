6d9433a8d640a16bde25b89e771924d6e34918fd
Mklingen <Mklingen@Mklingen-PC>
2014-06-06 19:37:22 -0700

    added states

---
+++ VoxelTest/VoxelTest/GameStates/EconomyState.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Color = Microsoft.Xna.Framework.Color;
+using Rectangle = Microsoft.Xna.Framework.Rectangle;
+
+namespace DwarfCorp.GameStates
+{
+
+    /// <summary>
+    /// This game state allows the player to buy/sell goods from a balloon with a drag/drop interface.
+    /// </summary>
+    public class EconomyState : GameState
+    {
+        public DwarfGUI GUI { get; set; }
+        public Drawer2D Drawer { get; set; }
+        public GUIComponent MainWindow { get; set; }
+        public int EdgePadding { get; set; }
+        public GridLayout Layout { get; set; }
+        public InputManager Input { get; set; }
+        public PlayState PlayState { get; set; }
+        public Texture2D Icons { get; set; }
+        public List<Button> TabButtons { get; set; }
+        public Dictionary<string, GUIComponent> Tabs { get; set; } 
+      
+
+        public EconomyState(DwarfGame game, GameStateManager stateManager, PlayState play) :
+            base(game, "EconomyState", stateManager)
+        {
+            
+            EdgePadding = 32;
+            Input = new InputManager();
+            PlayState = play;
+            EnableScreensaver = false;
+            InputManager.KeyReleasedCallback += InputManager_KeyReleasedCallback;
+           
+        }
+
+        void Initialize()
+        {
+            Tabs = new Dictionary<string, GUIComponent>();
+            Icons = TextureManager.GetTexture(ContentPaths.GUI.icons);
+            TabButtons = new List<Button>();
+            GUI = new DwarfGUI(Game, Game.Content.Load<SpriteFont>(ContentPaths.Fonts.Default), Game.Content.Load<SpriteFont>(ContentPaths.Fonts.Title), Game.Content.Load<SpriteFont>(ContentPaths.Fonts.Small), Input)
+            {
+                DebugDraw = false
+            };
+            IsInitialized = true;
+            Drawer = new Drawer2D(Game.Content, Game.GraphicsDevice);
+            MainWindow = new GUIComponent(GUI, GUI.RootComponent)
+            {
+                LocalBounds = new Rectangle(EdgePadding, EdgePadding, Game.GraphicsDevice.Viewport.Width - EdgePadding * 2, Game.GraphicsDevice.Viewport.Height - EdgePadding * 2)
+            };
+            Layout = new GridLayout(GUI, MainWindow, 11, 4);
+            Layout.UpdateSizes();
+
+            Button back = new Button(GUI, Layout, "Back", GUI.DefaultFont, Button.ButtonMode.PushButton, null);
+            Layout.SetComponentPosition(back, 3, 10, 1, 1);
+            back.OnClicked += back_OnClicked;
+
+            Panel tabPanel = new Panel(GUI, Layout);
+            Layout.SetComponentPosition(tabPanel, 0, 0, 4, 1);
+
+            GridLayout tabLayout = new GridLayout(GUI, tabPanel, 1, 10);
+
+
+           CreateTabButton(tabLayout, "Employees", "Hire and fire dwarves", 5, 0);
+            EmployeeDisplay employeeDisplay = new EmployeeDisplay(GUI, Layout, PlayState.Master.Faction)
+            {
+                IsVisible = false
+            };
+            Tabs["Employees"] = employeeDisplay;
+           
+           CreateTabButton(tabLayout, "Assets", "Buy/Sell goods", 3, 1);
+           GoodsPanel assetsPanel = new GoodsPanel(GUI, Layout, PlayState.Master.Faction)
+           {
+               IsVisible = false
+           };
+           Tabs["Assets"] = assetsPanel;
+
+
+           CreateTabButton(tabLayout, "Capital", "Financial report", 2, 1);
+           Panel capitalPanel = new Panel(GUI, Layout)
+           {
+               IsVisible = false
+           };
+           Tabs["Capital"] = capitalPanel;
+
+          ButtonClicked(TabButtons[0]);
+        }
+
+        public Button CreateTabButton(GridLayout parent, string name, string tooltip, int x, int y)
+        {
+            Button button = new Button(GUI, parent, name, GUI.DefaultFont, Button.ButtonMode.ImageButton, new ImageFrame(Icons, 32, x, y))
+            {
+                CanToggle = true,
+                IsToggled = false,
+                KeepAspectRatio = true,
+                DontMakeBigger = true,
+                ToolTip = tooltip,
+                TextColor = Color.Black,
+                HoverTextColor = Color.DarkRed,
+                DrawFrame = true
+            };
+            button.OnClicked += () => ButtonClicked(button);
+
+            parent.SetComponentPosition(button, TabButtons.Count, 0, 1, 1);
+            TabButtons.Add(button);
+
+            
+            
+            return button;
+        }
+
+        public void SetTab(string tabName)
+        {
+            foreach(KeyValuePair<string, GUIComponent> tab in Tabs)
+            {
+                if(tabName == tab.Key)
+                {
+
+                    Layout.SetComponentPosition(tab.Value, 0, 2, 4, 7);
+                    tab.Value.IsVisible = true;
+                }
+                else
+                {
+                    tab.Value.IsVisible = false;
+                }
+              
+            }
+        }
+
+        public void ButtonClicked(Button button)
+        {
+            foreach(Button b in TabButtons)
+            {
+                if(button != b)
+                {
+                    b.IsToggled = false;
+                }
+                else
+                {
+                    SetTab(b.Text);
+                }
+            }
+        }
+
+        void InputManager_KeyReleasedCallback(Microsoft.Xna.Framework.Input.Keys key)
+        {
+            if(!IsActiveState)
+            {
+                return;
+            }
+
+            if(key == Keys.Escape)
+            {
+                back_OnClicked();
+            }
+        }
+
+
+        public override void OnEnter()
+        {
+            PlayState.GUI.ToolTipManager.ToolTip = "";
+            Initialize();
+            base.OnEnter();
+        }
+
+     
+        private void back_OnClicked()
+        {
+            PlayState.Paused = false;
+            PlayState.GUI.RootComponent.IsVisible = true;
+            StateManager.PopState();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            MainWindow.LocalBounds = new Rectangle(EdgePadding, EdgePadding, Game.GraphicsDevice.Viewport.Width - EdgePadding * 2, Game.GraphicsDevice.Viewport.Height - EdgePadding * 2);
+            Input.Update();
+            GUI.Update(gameTime);
+            base.Update(gameTime);
+        }
+
+
+        private void DrawGUI(GameTime gameTime, float dx)
+        {
+            GUI.PreRender(gameTime, DwarfGame.SpriteBatch);
+            DwarfGame.SpriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend, SamplerState.PointClamp, DepthStencilState.None, RasterizerState.CullNone);
+
+            Drawer2D.FillRect(DwarfGame.SpriteBatch, Game.GraphicsDevice.Viewport.Bounds, new Color(0, 0, 0, 200));
+         
+            GUI.Render(gameTime, DwarfGame.SpriteBatch, new Vector2(dx, 0));
+
+            Drawer.Render(DwarfGame.SpriteBatch, null, Game.GraphicsDevice.Viewport);   
+
+            DwarfGame.SpriteBatch.End();
+            GUI.PostRender(gameTime);
+        }
+
+        public override void Render(GameTime gameTime)
+        {
+            DrawGUI(gameTime, 0);
+            base.Render(gameTime);
+        }
+    }
+
+}
