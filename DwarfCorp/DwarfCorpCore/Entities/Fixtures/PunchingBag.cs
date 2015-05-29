fd2e31cbab96954186c3ba8430a637cd432f0964
mklingen <mklingen@localhost>
2015-05-29 18:44:53 -0400

    Quickly added a couple of furnitures

---
+++ DwarfCorp/DwarfCorpCore/Entities/Fixtures/PunchingBag.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DwarfCorp.GameStates;
+using Microsoft.Xna.Framework;
+using Newtonsoft.Json;
+
+namespace DwarfCorp
+{
+    [JsonObject(IsReference = true)]
+    public class PunchingBag : Fixture
+    {
+        public PunchingBag()
+        {
+
+        }
+
+        public PunchingBag(Vector3 position) :
+            base(position, new SpriteSheet(ContentPaths.Entities.Furniture.interior_furniture, 32, 32), new Point(2, 5), PlayState.ComponentManager.RootComponent)
+        {
+            Name = "PunchingBag";
+            Tags.Add("PunchingBag");
+            Tags.Add("Train");
+        }
+    }
+}
---
+++ DwarfCorp/DwarfCorpCore/Entities/Fixtures/WeightRack.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DwarfCorp.GameStates;
+using Microsoft.Xna.Framework;
+using Newtonsoft.Json;
+
+namespace DwarfCorp
+{
+    [JsonObject(IsReference = true)]
+    public class WeightRack : Fixture
+    {
+        public WeightRack()
+        {
+
+        }
+
+        public WeightRack(Vector3 position) :
+            base(position, new SpriteSheet(ContentPaths.Entities.Furniture.interior_furniture, 32, 32), new Point(3, 5), PlayState.ComponentManager.RootComponent)
+        {
+            Name = "WeightRack";
+            Tags.Add("WeightRack");
+            Tags.Add("Train");
+        }
+    }
+}
