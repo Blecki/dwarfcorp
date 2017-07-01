a471f840502b39d27ff7de71a8dd3cce8bfc0649
mklingen <mklingen89@gmail.com>
2017-07-01 11:37:53 -0400

    auto-sync FNA and XNA. Add deployments for Itch and Steam

---
+++ deploy_itch.bat
@@ -0,0 +1,6 @@
+butler login
+butler push .\\DwarfCorp\\DwarfCorpXNA\\build\\osx completelyfairgames/dwarfcorp:osx
+butler push .\\DwarfCorp\\DwarfCorpXNA\\build\\win32 completelyfairgames/dwarfcorp:win32-fna
+butler push .\\DwarfCorp\\DwarfCorpXNA\\build\\linux64 completelyfairgames/dwarfcorp:linux64
+butler push .\\DwarfCorp\\DwarfCorpXNA\\build\\linux32 completelyfairgames/dwarfcorp:linux32
+butler push .\\DwarfCorp\\DwarfCorpXNA\\bin\\x86\\Release completelyfairgames/dwarfcorp:win32-xna
