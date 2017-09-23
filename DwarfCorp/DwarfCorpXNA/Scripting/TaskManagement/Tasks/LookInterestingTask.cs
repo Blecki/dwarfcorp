d04fba6b5f99cfd02883cb039a9043284a23c67b
mklingen <mklingen89@gmail.com>
2017-09-23 10:54:29 -0400

    horrible hack to prevent dwarves from gathering on ladders for conversation

--- DwarfCorp/DwarfCorpXNA/Scripting/TaskManagement/Tasks/LookInterestingTask.cs
+++ DwarfCorp/DwarfCorpXNA/Scripting/TaskManagement/Tasks/LookInterestingTask.cs
@@ -107,13 +107,11 @@ namespace DwarfCorp
                 return new GoToZoneAct(creature.AI, Datastructures.SelectRandom(rooms));
             }
 
-            /*
             bool goToItem = MathFunctions.RandEvent(0.2f);
             if (goToItem && items.Count > 0)
             {
-                return new GoToEntityAct(Datastructures.SelectRandom(items), creature.AI) & new Wrap(() => ConverseFriends(creature.AI));
+                return new GoToEntityAct(Datastructures.SelectRandom(items.Where(item => !item.Tags.Contains("Climbable"))), creature.AI) & new Wrap(() => ConverseFriends(creature.AI));
             }
-            */
 
             bool goToMinion = MathFunctions.RandEvent(0.2f);
             if (goToMinion && minions.Count > 1)
