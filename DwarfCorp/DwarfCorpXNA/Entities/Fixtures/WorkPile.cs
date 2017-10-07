31aac4500d0217e405123a1069c1e0cb28f98b6a
Blecki <jm@omnisu.com>
2017-10-06 22:35:03 -0400

    Finish auditting fixtures

--- DwarfCorp/DwarfCorpXNA/Entities/Fixtures/WorkPile.cs
+++ DwarfCorp/DwarfCorpXNA/Entities/Fixtures/WorkPile.cs
@@ -40,7 +40,6 @@ using Newtonsoft.Json;
 
 namespace DwarfCorp
 {
-    [JsonObject(IsReference = true)]
     public class WorkPile : Fixture
     {
         public WorkPile()
@@ -51,95 +50,6 @@ namespace DwarfCorp
         public WorkPile(ComponentManager componentManager, Vector3 position) :
             base(componentManager, position, new SpriteSheet(ContentPaths.Entities.DwarfObjects.underconstruction, 32, 32), new Point(0, 0))
         {
-        }
-    }
-
-    [JsonObject(IsReference = true)]
-    public class Fence : Fixture
-    {
-        public float FenceRotation { get; set; }
-        public Fence()
-        {
-            
-        }
-
-        public Fence(ComponentManager componentManager, Vector3 position, float orientation, string asset) :
-            base(componentManager, position, new SpriteSheet(asset, 32, 32), new Point(0, 0))
-        {
-            FenceRotation = orientation;
-            GetComponent<SimpleSprite>().OrientationType = SimpleSprite.OrientMode.Fixed;
-            GetComponent<SimpleSprite>().LocalTransform = Matrix.CreateRotationY(FenceRotation);
-        }
-
-
-        public override void CreateCosmeticChildren(ComponentManager manager)
-        {
-            base.CreateCosmeticChildren(manager);
-            GetComponent<SimpleSprite>().OrientationType = SimpleSprite.OrientMode.Fixed;
-            GetComponent<SimpleSprite>().LocalTransform = Matrix.CreateRotationY(FenceRotation);
-        }
-
-        private struct FenceSegmentInfo
-        {
-            public GlobalVoxelOffset VoxelOffset;
-            public Vector3 VisibleOffset;
-            public float Angle;
-        }
-
-        private static FenceSegmentInfo[] FenceSegments = new FenceSegmentInfo[]
-        {
-            new FenceSegmentInfo
-            {
-                VoxelOffset = new GlobalVoxelOffset(0,0,1),
-                VisibleOffset = new Microsoft.Xna.Framework.Vector3(0,0,0.45f),
-                Angle = (float)Math.Atan2(0,1)
-            },
-
-            new FenceSegmentInfo
-            {
-                VoxelOffset = new GlobalVoxelOffset(0,0,-1),
-                VisibleOffset = new Microsoft.Xna.Framework.Vector3(0,0,-0.45f),
-                Angle = (float)Math.Atan2(0,-1)
-            },
-
-            new FenceSegmentInfo
-            {
-                VoxelOffset = new GlobalVoxelOffset(1,0,0),
-                VisibleOffset = new Microsoft.Xna.Framework.Vector3(0.45f,0,0),
-                Angle = (float)Math.Atan2(1,0)
-            },
-
-            new FenceSegmentInfo
-            {
-                VoxelOffset = new GlobalVoxelOffset(-1,0,0),
-                VisibleOffset = new Microsoft.Xna.Framework.Vector3(-0.45f,0,0),
-                Angle = (float)Math.Atan2(-1,0)
-            },
-        };
-
-        public static IEnumerable<Body> CreateFences(
-            ComponentManager components,
-            string asset, 
-            IEnumerable<VoxelHandle> Voxels, 
-            bool createWorkPiles)
-        {
-            Vector3 off = (Vector3.One * 0.5f) + Vector3.Up;
-            foreach (var voxel in Voxels)
-            {
-                foreach (var segment in FenceSegments)
-                {
-                    var neighbor = VoxelHelpers.GetNeighbor(voxel, segment.VoxelOffset);
-                    if (neighbor.IsValid && !Voxels.Any(v => v == neighbor))
-                        yield return new Fence(components,
-                            voxel.WorldPosition + off + segment.VisibleOffset,
-                            segment.Angle, asset);
-                }
-
-                if (createWorkPiles && MathFunctions.RandEvent(0.1f))
-                {
-                    yield return new WorkPile(components, voxel.WorldPosition + off);
-                }
-            }
         }
     }
 }
