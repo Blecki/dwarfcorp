8773ca67473db63b9ac3378f03c40a0014fbe987
Blecki <jm@omnisu.com>
2017-10-21 10:00:48 -0400

    Fix wall and floor building

--- DwarfCorp/DwarfCorpXNA/GameStates/PlayState.cs
+++ DwarfCorp/DwarfCorpXNA/GameStates/PlayState.cs
@@ -439,7 +439,7 @@ namespace DwarfCorp.GameStates
 
             int newNum = Math.Max(factionResources[data.ResourceToRelease].First.NumResources -
                 World.PlayerFaction.Designations.EnumerateDesignations(DesignationType.Put).Count(d =>
-                    (d.Tag as VoxelType).ResourceToRelease == data.ResourceToRelease), 0);
+                    VoxelLibrary.GetVoxelType((d.Tag as short?).Value).ResourceToRelease == data.ResourceToRelease), 0);
 
             if (newNum != numResources)
             {
