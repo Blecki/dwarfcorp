25ad1585d6d3f5b1d26b40d2ed47e77b3055ba12
Matt Klingensmith <mklingen89@gmail.com>
2018-02-04 11:15:15 -0500

    quieter creature tantrum when bade trade

--- DwarfCorp/DwarfCorpXNA/LanguageGenerator/Language.cs
+++ DwarfCorp/DwarfCorpXNA/LanguageGenerator/Language.cs
@@ -77,7 +77,7 @@ namespace DwarfCorp
         {
             if (Yays != null && Yays.Count > 0)
             {
-                SoundManager.PlaySound(Datastructures.SelectRandom(Yays), 0.25f);
+                SoundManager.PlaySound(Datastructures.SelectRandom(Yays), 0.15f);
             }
         }
 
@@ -85,7 +85,7 @@ namespace DwarfCorp
         {
             if (Boos != null && Boos.Count > 0)
             {
-                SoundManager.PlaySound(Datastructures.SelectRandom(Boos), 0.25f);
+                SoundManager.PlaySound(Datastructures.SelectRandom(Boos), 0.15f);
             }
         }
 
