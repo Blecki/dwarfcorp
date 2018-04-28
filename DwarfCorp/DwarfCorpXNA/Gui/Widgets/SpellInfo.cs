5c379aa0a79af0f3160f1127152113b16ccf79cf
mklingen <mklingen89@gmail.com>
2018-04-28 18:02:58 -0400

    fix magic gui

--- DwarfCorp/DwarfCorpXNA/Gui/Widgets/SpellInfo.cs
+++ DwarfCorp/DwarfCorpXNA/Gui/Widgets/SpellInfo.cs
@@ -14,29 +14,45 @@ namespace DwarfCorp.Gui.Widgets
     {
         public SpellTree.Node Spell;
         public GameMaster Master;
-
+        private int _lastProgress = -1;
         public override void Construct()
         {
+            this.Root.RegisterForUpdate(this);
             Border = "border-fancy";
 
-            var builder = new StringBuilder();
-            builder.AppendLine(Spell.Spell.Name);
-            builder.AppendLine(Spell.Spell.Description);
-            if (Spell.IsResearched)
-            {
-                builder.AppendLine("\n-- Click to cast");
-            }
-            else if (Spell.Children.Count > 0)
-            {
-                if (Spell.ResearchProgress > 0)
-                {
-                    builder.AppendLine(string.Format("{0} % researched", (int) (Spell.ResearchProgress*100)));
-                }
-                builder.AppendLine("Unlocks:");
-                builder.AppendLine(TextGenerator.GetListString(Spell.Children.Select(child => child.Spell.Name)));
-            }
             Font = "font8";
-            Text = builder.ToString();
+            Update();
+            OnUpdate = (widget, time) => this.Update();
+        }
+
+        public void Update()
+        {
+            if (Hidden)
+            {
+                return;
+            }
+            int progress = (int)(100 * Spell.ResearchProgress / Spell.ResearchTime);
+            if (progress != _lastProgress)
+            {
+                _lastProgress = progress;
+                var builder = new StringBuilder();
+                builder.AppendLine(Spell.Spell.Name);
+                builder.AppendLine(Spell.Spell.Description);
+                if (Spell.IsResearched)
+                {
+                    builder.AppendLine("\n-- Click to cast");
+                }
+                else if (Spell.Children.Count > 0)
+                {
+                    if (Spell.ResearchProgress > 0)
+                    {
+                        builder.AppendLine(string.Format("{0} % researched", (int)(100 * Spell.ResearchProgress / Spell.ResearchTime)));
+                    }
+                    builder.AppendLine("Unlocks:");
+                    builder.AppendLine(TextGenerator.GetListString(Spell.Children.Select(child => child.Spell.Name)));
+                }
+                Text = builder.ToString();
+            }
         }
     }
 }
