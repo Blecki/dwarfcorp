afaceb395d0af69664add78d5161170817c960e2
Blecki <jm@omnisu.com>
2018-05-06 17:03:37 -0400

    .

--- DwarfCorp/DwarfCorpXNA/Graphics/Instancing/InstanceRenderer.cs
+++ DwarfCorp/DwarfCorpXNA/Graphics/Instancing/InstanceRenderer.cs
@@ -1,10 +1,4 @@
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using DwarfCorp.GameStates;
-using LibNoise.Modifiers;
-using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Content;
 
