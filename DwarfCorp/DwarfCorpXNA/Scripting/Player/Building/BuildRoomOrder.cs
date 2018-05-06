126d7ff81be908758ac356a09c2a3767604567a8
mklingen <mklingen89@gmail.com>
2018-05-06 15:24:35 -0400

    remove zone num in announcement

--- DwarfCorp/DwarfCorpXNA/Scripting/Player/Building/BuildRoomOrder.cs
+++ DwarfCorp/DwarfCorpXNA/Scripting/Player/Building/BuildRoomOrder.cs
@@ -151,7 +151,7 @@ namespace DwarfCorp
 
             if (!silent)
             {
-                World.MakeAnnouncement(String.Format("{0} was built", ToBuild.ID), null);
+                World.MakeAnnouncement(String.Format("{0} was built", ToBuild.RoomData.Name), null);
                 SoundManager.PlaySound(ContentPaths.Audio.Oscar.sfx_gui_positive_generic, 0.15f);
                 World.GoalManager.OnGameEvent(new Goals.Events.BuiltRoom(ToBuild.ID));
             }
