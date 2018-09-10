063b5069d492206dfeb77e951c973d03edfad00d
mklingen <mklingen89@gmail.com>
2018-09-10 16:58:30 -0400

    more stuff can happen to adventuring parties now

--- DwarfCorp/DwarfCorpXNA/Scripting/Adventure/Adventure.cs
+++ DwarfCorp/DwarfCorpXNA/Scripting/Adventure/Adventure.cs
@@ -250,6 +250,59 @@ namespace DwarfCorp.Scripting.Adventure
             }
             return 24 + diff;
         }
+
+        public bool MaybeCapture(WorldManager world)
+        {
+            Faction owner = world.Factions.Factions[OwnerFaction];
+            Faction target = world.Factions.Factions[DestinationFaction];
+            var politics = world.Diplomacy.GetPolitics(owner, target);
+            if (politics.GetCurrentRelationship() != Relationship.Hateful)
+            {
+                return false;
+            }
+
+            if (!MathFunctions.RandEvent(0.1f))
+            {
+                return false;
+            }
+
+            float strength = Party.Sum(p => p.Stats.BuffedStr);
+            float enemyStrength = MathFunctions.RandInt(5, 50);
+            if (enemyStrength > strength)
+            {
+                int numCaptured = MathFunctions.RandInt(1, Party.Count);
+                for (int i = 0; i < numCaptured; i++)
+                {
+                    int j = MathFunctions.RandInt(0, Party.Count);
+                    Party[j].Delete();
+                    Party.RemoveAt(j);
+                }
+                LastEvent = String.Format("{0} dwarf(s) in the adventuring party were captured by {1}!", numCaptured, target.Race.Plural);
+                return true;
+            }
+            return false;
+        }
+
+        public bool MaybeStorm(WorldManager world)
+        {
+            if (!MathFunctions.RandEvent(0.05f))
+            {
+                return false;
+            }
+            int randomDelay = MathFunctions.RandInt(1, 12);
+            RemainingTravelTime += new TimeSpan(0, randomDelay, 0, 0, 0);
+            LastEvent = String.Format("A storm has delayed the adventuring party by {0} hours.", randomDelay);
+            return true;
+        }
+
+        public void MaybeDelay(WorldManager world)
+        {
+            if (MaybeCapture(world))
+                return;
+            if (MaybeStorm(world))
+                return;
+        }
+
         private int _prevHour = -1;
 
         public void Update(WorldManager world, DwarfTime time)
@@ -264,6 +317,7 @@ namespace DwarfCorp.Scripting.Adventure
             if (timeDiff > 0)
             {
                 _prevHour = hour;
+                MaybeDelay(world);
             }
             switch (AdventureState)
             {
