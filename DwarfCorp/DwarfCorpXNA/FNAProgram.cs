461e8a3d30221742bfdae6b3631695365f074b17
mklingen <mklingen89@gmail.com>
2018-09-16 15:26:33 -0400

    18.09.16

--- DwarfCorp/DwarfCorpXNA/FNAProgram.cs
+++ DwarfCorp/DwarfCorpXNA/FNAProgram.cs
@@ -53,8 +53,8 @@ namespace DwarfCorp
 #if WINDOWS || XBOX
     internal static class Program
     {
-        public static string Version = "18.08.19_FNA";
-        public static string[] CompatibleVersions = { "18.08.19_FNA", "18.08.19_XNA", "18.08.05_FNA", "18.08.05_XNA" };
+        public static string Version = "18.09.16_FNA";
+        public static string[] CompatibleVersions = { "18.09.16_FNA", "18.09.03_FNA"};
         public static string Commit = "UNKNOWN";
         public static char DirChar = Path.DirectorySeparatorChar;
         
