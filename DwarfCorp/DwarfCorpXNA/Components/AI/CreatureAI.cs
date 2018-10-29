a33aa4e08374dd945e87723b4d8406c55982b975
mklingen <mklingen89@gmail.com>
2018-10-28 20:15:40 -0400

    kill creatures that cannot swim when they are in water

--- DwarfCorp/DwarfCorpXNA/Components/AI/CreatureAI.cs
+++ DwarfCorp/DwarfCorpXNA/Components/AI/CreatureAI.cs
@@ -775,10 +775,12 @@ namespace DwarfCorp
             if (MathFunctions.RandEvent(0.01f))
             {
                 var above = VoxelHelpers.GetVoxelAbove(Physics.CurrentVoxel);
-                bool shouldDrown = above.IsValid && (!above.IsEmpty || above.LiquidLevel > 0);
-                if (Physics.IsInLiquid && (!Movement.CanSwim || shouldDrown))
+                var below = VoxelHelpers.GetVoxelBelow(Physics.CurrentVoxel);
+                bool shouldDrown = (above.IsValid && (!above.IsEmpty || above.LiquidLevel > 0));
+                if ((Physics.IsInLiquid || (!Movement.CanSwim && (below.IsValid && (below.LiquidLevel > 5)))) 
+                    && (!Movement.CanSwim || shouldDrown))
                 {
-                    Creature.Damage(1.0f, Health.DamageType.Normal);
+                    Creature.Damage(Movement.CanSwim ? 1.0f : 30.0f, Health.DamageType.Normal);
                 }
             }
 
